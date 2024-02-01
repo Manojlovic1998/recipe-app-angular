604f166390ac8fe9884e529da13c703bb0e78dfb
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-02-01 09:37:29 +0100

    Add logic to generate ingredients for recipes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [];
 
+  constructor() {
+    this.generateIngredients();
+  }
+
+  private generateIngredients() {
+    for (let i = 0; i < 3; i++) {
+      this.ingredients.push(this.generateIngredient());
+    }
+  }
+
+  private generateIngredient() {
+    return new Ingredient(
+      faker.lorem.word(5),
+      faker.number.int({ min: 1, max: 10 })
+    );
+  }
 }
