3593e0abf56f39480cb6d5d519919c99afbd30c6
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-02-01 09:13:29 +0100

    Add recipe list context structure and faker data set

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [];
 
+  constructor() {
+    this.generateRecipes();
+  }
+
+  private generateRecipes() {
+    for (let i = 0; i < 3; i++) {
+      this.recipes.push(this.generateRecipe());
+    }
+  }
+
+  private generateRecipe() {
+    return new Recipe(
+      faker.lorem.words(),
+      faker.lorem.paragraph(),
+      faker.image.urlLoremFlickr({ category: 'food', width: 300, height: 300 })
+    );
+  }
 }
