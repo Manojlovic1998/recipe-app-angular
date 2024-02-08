78bc5db695f5bd5efd45182fec489b42f02e10c3
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-02-09 00:56:08 +0100

    Add shopping-list service to handle ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,33 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { faker } from '@faker-js/faker';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [];
 
-  constructor() {
-    this.generateIngredients();
-  }
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
-
-  private generateIngredients() {
-    for (let i = 0; i < 3; i++) {
-      this.ingredients.push(this.generateIngredient());
-    }
-  }
-
-  private generateIngredient() {
-    return new Ingredient(
-      faker.lorem.word(5),
-      faker.number.int({ min: 1, max: 10 })
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientAdded.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
     );
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,35 @@
-export class ShoppingListService {}
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { faker } from '@faker-js/faker';
+
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [];
+  ingredientAdded = new EventEmitter<Ingredient[]>();
+
+  constructor() {
+    this.generateIngredients();
+  }
+
+  getIngredients() {
+    // New array with the same elements
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientAdded.emit(this.ingredients.slice());
+  }
+
+  private generateIngredients() {
+    for (let i = 0; i < 3; i++) {
+      this.ingredients.push(this.generateIngredient());
+    }
+  }
+
+  private generateIngredient() {
+    return new Ingredient(
+      faker.lorem.word(5),
+      faker.number.int({ min: 1, max: 10 })
+    );
+  }
+}
