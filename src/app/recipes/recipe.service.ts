1e34c0ed85ac261905b64fb9eedaabbcad205d63
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-02-09 21:15:25 +0100

    Add func to display recipe's ingredients on select

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,26 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { faker } from '@faker-js/faker';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [];
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {
+  constructor(private slService: ShoppingListService) {
     this.generateRecipes();
   }
 
   getRecipes() {
     // New array with the same elements
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    // Add ingredients to shopping list
+    this.slService.addIngredients(ingredients);
   }
 
   private generateRecipes() {
@@ -25,7 +33,24 @@ export class RecipeService {
     return new Recipe(
       faker.lorem.words(),
       faker.lorem.paragraph(),
-      faker.image.urlLoremFlickr({ category: 'food', width: 300, height: 300 })
+      faker.image.urlLoremFlickr({ category: 'food', width: 300, height: 300 }),
+      this.generateIngredients()
+    );
+  }
+
+  private generateIngredients() {
+    const ingredients: Ingredient[] = [];
+    for (let i = 0; i < 3; i++) {
+      ingredients.push(this.generateIngredient());
+    }
+
+    return ingredients;
+  }
+
+  private generateIngredient() {
+    return new Ingredient(
+      faker.lorem.word(5),
+      faker.number.int({ min: 1, max: 10 })
     );
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -6,9 +6,7 @@ export class ShoppingListService {
   private ingredients: Ingredient[] = [];
   ingredientAdded = new EventEmitter<Ingredient[]>();
 
-  constructor() {
-    this.generateIngredients();
-  }
+  constructor() {}
 
   getIngredients() {
     // New array with the same elements
@@ -20,16 +18,11 @@ export class ShoppingListService {
     this.ingredientAdded.emit(this.ingredients.slice());
   }
 
-  private generateIngredients() {
-    for (let i = 0; i < 3; i++) {
-      this.ingredients.push(this.generateIngredient());
-    }
-  }
-
-  private generateIngredient() {
-    return new Ingredient(
-      faker.lorem.word(5),
-      faker.number.int({ min: 1, max: 10 })
-    );
+  addIngredients(ingredients: Ingredient[]) {
+    // Remove all elements from the array
+    this.ingredients = [];
+    // Add ingredients to shopping list
+    this.ingredients.push(...ingredients);
+    this.ingredientAdded.emit(this.ingredients.slice());
   }
 }
