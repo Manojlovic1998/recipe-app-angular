f27c6c0e2d1119a74b9fbc5cc941e8cbad4aebd0
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-05-23 00:31:30 +0200

    Amend Create CoreModule to envelop apps core services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { PermissionService } from './auth/auth.guard';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core.module';
@@ -21,7 +17,6 @@ import { CoreModule } from './core.module';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     CoreModule,
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,3 +1,10 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { PermissionService } from './auth/auth.guard';
+import { NgModule } from '@angular/core';
+
 @NgModule({
   providers: [
     ShoppingListService,
